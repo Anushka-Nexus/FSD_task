be757e888109ea477ae7273367044e08333358aa
Anushka-Nexus <shendeanushka1@gmail.com>
2025-08-11 12:32:41 +0530

    added for-loop summary and task

--- JAVASCRIPT/LESSONS/3.Statements/for_loop.js
+++ JAVASCRIPT/LESSONS/3.Statements/for_loop.js
@@ -1,41 +1,53 @@
-//wap to print a factorial of a number.
+//wap to recreate a do-while loop
 
-let num = 8
-let ans = 1
+let choice = false
+      do{
+         let number=Number(window.prompt("enter any number to check whether the number is palindrome or not :-"))
+         let o_num=number
+         let rev=0
+         let l_digit=null
 
-while (num > 0) {
-    ans = ans * num
-    num--
-}
-console.log("Factorial is :-" + ans)
+         while(number!=0){
+            l_digit=number%10
+            rev = rev*10+l_digit
+            number=parseInt(number/10)
+         }
+         if(o_num==rev){
+            console.log(o_num+" is a palindrome number")
+         }
+         else{
+            console.log(o_num+ " is not a palindrome number") 
+         }
+         choice=window.confirm("do you wish to continue now?")
+      }while(choice)
 
-//wap using while loop to print even numbers from 0 to 50
+//wap to find even numbers from range 1 to 50 
+
 console.log("Even no. from the range 1 to 50 are :-")
-let num1 = 0
-while (num1 <= 50) {
+for(let num1=0;num1 <= 50;num1++) {
     if (num1 % 2 == 0) {
         console.log(num1)
     }
-    num1++
+
 }
 
 // wap to print odd numbers 1 to 50
 console.log("Odd no. from the range 1 to 50 are :-")
-let number = 1
-while (number <= 50) {
+
+for(let number=1;number <= 50;number++) {
     if (number % 2 != 0) {
         console.log(number)
     }
-    number++
+
 }
 
 //wap to find out whether the given no. is armstrong or not also from range 1 to 500
 
-let range = 100
+
 let count_armstrong = 0
 
 console.log("The list of armstrong number:-")
-while (range <= 500) {
+ for (let range = 100 ; range <= 500 ; range++) {
     let copy_num = range;
     let sum = 0;
 
@@ -50,19 +62,18 @@ while (range <= 500) {
         count_armstrong++;
     }
     
-    range++;
+    
 }
 console.log("There are total " + count_armstrong + " armstrong numbers from range 100 to 500");
 
 //wap to find out reverse of the numbers from the range 50 to 200 also check whether the given number is palindrome or not.
 //reverse-num = reverse-num * 10 + last-digit
 
-let start_num = 50
 let count_palindrome = 0
 let lastDigit = 0
 
 console.log("The list of palindrome number:-")
-while (start_num <= 200) {
+for(let start_num = 50 ;start_num <= 200;start_num++) {
     let original = start_num;
     let reverse = 0;
 
@@ -76,21 +87,21 @@ while (start_num <= 200) {
         console.log(start_num + " is a Palindrome number");
         count_palindrome++
     }
-    start_num++
+    
 }
 console.log("There are total " + count_palindrome + " palindrome numbers from range 50-200")
 
 //wap to find out prime number from range 10 to 250
 //prime number is only divisble by 1 and itself
 
-let p_num=2
+
 let i =2
 let p_count=0
 let check=0
-while(p_num<=250){
+for(let p_num=2;p_num<=250; p_num++){
     i=2 //reset 
     check=0 //reset
-    while(i<p_num){
+    while(i*i<=p_num){
         if(p_num % i == 0){
            check=1
         }
@@ -100,10 +111,8 @@ while(p_num<=250){
         console.log(p_num+" is a prime")
         p_count++
     }
-    p_num++
+   
 }
 console.log("There are total "+p_count+" prime numbers from 10 to 250")
-
-
 
 
