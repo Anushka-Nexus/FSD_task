831dc15f9fd680c0d73508fa9adede8ce81c8cfb
Anushka-Nexus <shendeanushka1@gmail.com>
2025-09-10 23:23:39 +0530

    added delete & update

--- JAVASCRIPT/LESSONS/13.DOM/Events/script.js
+++ JAVASCRIPT/LESSONS/13.DOM/Events/script.js
@@ -1,6 +1,7 @@
 let store = []
 let form_target = document.getElementById("customform")
 let table_target = document.getElementById("table")
+let editIndex=null
 
 form_target.addEventListener("submit", (e) => {
     e.preventDefault()
@@ -8,42 +9,92 @@ form_target.addEventListener("submit", (e) => {
 })
 
 function data_fuctn(e) {
-    store.push(
-        {
+     if (editIndex === null) {
+        store.push({
             work: e.target.taskname.value,
             date: e.target.due.value,
-            priority :document.querySelector('input[name="priorty_radiobtn"]:checked').value,
+            priority: document.querySelector('input[name="priorty_radiobtn"]:checked').value,
             Description: e.target.description.value
-        })
-    createtable()
+        });
+    } else {
+        store[editIndex] = {
+            work: e.target.taskname.value,
+            date: e.target.due.value,
+            priority: document.querySelector('input[name="priorty_radiobtn"]:checked').value,
+            Description: e.target.description.value
+        };
+        editIndex = null;
+    }
+
+    e.target.reset();
+    createtable();
 }
+    
+      
 function createtable() {
-    let date = new Date().toLocaleDateString()
-    let time = new Date().toLocaleTimeString()
-    let data= store[store.length-1]
-    let newRow = document.createElement('tr');
-    let col1 = document.createElement('td');
-     col1.textContent = data.work;
-    let col2 = document.createElement('td');
-    col2.textContent = data.date;
-    let col3 = document.createElement('td');
-    col3.textContent = data.priority;
-    let col4 = document.createElement('td');
-    col4.textContent = data.Description;
-    let col5 = document.createElement('td');
-    col5.textContent= date
-    let col6 = document.createElement('td');
-    col6.textContent= time
+    let table_body = document.querySelector("tbody")
+    table_body.innerHTML="" //clear krega previous row ko
+    store.forEach((data, index) => {
+        let date = new Date().toLocaleDateString()
+        let time = new Date().toLocaleTimeString()
+      
+      
+        let newRow = document.createElement('tr');
+        let col1 = document.createElement('td');
+        col1.textContent = data.work;
+        let col2 = document.createElement('td');
+        col2.textContent = data.date;
+        let col3 = document.createElement('td');
+        col3.textContent = data.priority;
+        let col4 = document.createElement('td');
+        col4.textContent = data.Description;
+        let col5 = document.createElement('td');
+        col5.textContent = date
+        let col6 = document.createElement('td');
+        col6.textContent = time
+        let col7 = document.createElement(`td`);
+        // DELETE button
+        let delbtn = document.createElement("button");
+        delbtn.textContent = 'DELETE';
+        delbtn.onclick = () => {
+            deleteEntry(index);
+        };
 
+        // UPDATE button 
+        let updatebtn = document.createElement("button");
+        updatebtn.textContent = "UPDATE";
+        updatebtn.onclick = () => {
+            updateEntry(index);
+        };
+    
 
-   
+    col7.appendChild(delbtn)
+    col7.appendChild(updatebtn)
+
     newRow.appendChild(col1)
     newRow.appendChild(col2)
     newRow.appendChild(col3)
     newRow.appendChild(col4)
     newRow.appendChild(col5)
     newRow.appendChild(col6)
-    
-    table_target.style.display="block"
+    newRow.appendChild(col7)
+
+    table_target.style.display = "block"
     table_target.querySelector("tbody").appendChild(newRow);
+    })
+}
+
+function deleteEntry(delindex) {
+    store = store.filter((entry, index) => { return index != delindex })
+    createtable()
+}
+function updateEntry(uindex){
+     editIndex = uindex; 
+     editForm(store[uindex]);
+}
+function editForm(data) {
+    form_target.taskname.value = data.work;
+    form_target.due.value = data.date;
+    form_target.description.value = data.Description;
+    document.querySelector(`input[name="priorty_radiobtn"][value="${data.priority}"]`).checked = true;
 }
