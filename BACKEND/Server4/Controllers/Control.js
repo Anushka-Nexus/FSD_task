c63d2a45b72ef101dae1e9bb88f0b8cc4cdabf66
Anushka-Nexus <shendeanushka1@gmail.com>
2025-11-05 09:40:56 +0530

    added otp send logic

--- BACKEND/Server4/Controllers/Control.js
+++ BACKEND/Server4/Controllers/Control.js
@@ -1,10 +1,39 @@
 import express from "express"
 import {UserModel} from "../Models/UserSchema.js"
+import nodemailer from "nodemailer"
 
+ const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,          
+  secure: true,       // true for port 465
+  auth: {
+    user: process.env.USER_EMAIL,
+    pass: process.env.USER_PASSKEY,
+  },
+});
+ 
+let GenerateOTP =()=>{
+   return OTPGENERATE = Math.floor((Math.random()*9000)+1000)
+}
+
+let SendMail=async()=>{
+   let otp=GenerateOTP()
+   let SendedMailResult=nodemailer.sendMail({
+      from :process.env.USER_EMAIL,
+      to:email,
+      subject :"Complete Your Sign-In with Secured OTP",
+      html:`<div style="font-family:sans-serif; background: #fff; padding: 20px; border: 1px solid #eee; border-radius: 6px; margin: auto; text-align: center;">
+     <h2 style="color: #333; margin-bottom: 10px;">Verification Code</h2>
+      <p style="color: #555; font-size: 14px;">Use the OTP below to verify your email:</p>'
+      <h1 style="color: #e63946; margin: 15px 0;">${otp}</h1>
+     </div>`
+   })
+}
 let PostUserRegister= async(request,response)=>{
     try{
        let { Name, Phone, Email, Address, Password } = request.body
 
+      //validating whether user has given all the required data
      if(!Name || !Phone || !Email|| !Address|| !Password) throw("Please fill all required the information!")
 
      //checking if the user already exist
@@ -12,7 +41,7 @@ let PostUserRegister= async(request,response)=>{
         {
             $or: [
                 {
-                 "Email.Email": Email //Email.Email Email object ki Email
+                 "Email.Email": Email //Email.Email--> Email object key Email
                  },
                   {
                      "Phone": Phone 
@@ -26,10 +55,12 @@ let PostUserRegister= async(request,response)=>{
          let EmailObject = {
             Email: Email, verified: false
         }
-
+      //Register new user
         let NewUser = new UserModel({ Name, Phone, Email:EmailObject, Address, Password })
 
+        //saving details
         await NewUser.save()
+        
         
          response.status(202).json(
             { message: "User has been registered successfuly!" }
--- BACKEND/Server4/server.js
+++ BACKEND/Server4/server.js
@@ -1,19 +1,27 @@
 import express from "express"
 import dotenv from "dotenv"
+
+//connecting routes
 import {routes} from "./Router/Routes.js"
+//connecting db
 import "./Database/Connection.js"
 
 dotenv.config({ path: "./config.env" })
 let port = process.env.PORT 
 
+//instance of express
 const app= express()
 
 app.use(express.static("public"))
+
+//reads form data
 app.use(express.urlencoded({ extended: true }))
+
+//to parse the json onto the server
 app.use(express.json())
 
-
- app.use("/APIs", routes)
+//endpoint
+app.use("/APIs", routes)
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
