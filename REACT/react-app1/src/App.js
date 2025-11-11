a9b28c392e3f6d5f6db36b5da8462b64702a78fe
Anushka-Nexus <shendeanushka1@gmail.com>
2025-11-11 09:57:28 +0530

    added cra react app

---
+++ REACT/react-app1/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>This is heading</h1>
+      <ul>
+        <li>list 1</li>
+        <li>list 2</li>
+        <li>list 3</li>
+        <li>list 4</li>
+        <li>list 5</li>
+      </ul>
+    </div>
+  )
+}
+
+export default App;
