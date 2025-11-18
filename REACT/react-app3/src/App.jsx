b0c1e4703fc722b09218bff0090d6b38a178f16f
Anushka-Nexus <shendeanushka1@gmail.com>
2025-11-19 05:25:50 +0530

    added tailwind and state variable

--- REACT/react-app3/src/App.jsx
+++ REACT/react-app3/src/App.jsx
@@ -4,6 +4,8 @@ import "../node_modules/bootstrap/dist/js/bootstrap.bundle.js"
 import './Stylesheet/index.scss'
 import Com1 from './Components/component1.jsx'
 import Comp2 from './Components/component2.jsx'
+
+
 function App() {
    let [toShow, setToShow] = useState(false)
    const handleClick = () => {
@@ -12,7 +14,7 @@ function App() {
   return <>
     <div>
       <div>
-           <h3>this is app.jsx page content</h3>
+           <h3 className='text-3xl text-red-700'>this is app.jsx page content</h3>
       </div>
       {/* <Com1 />
       <Comp2 /> */}
