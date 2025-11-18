29924a963bc8680cdb61718a8102004a4eb9b95a
Anushka-Nexus <shendeanushka1@gmail.com>
2025-11-18 08:22:34 +0530

    conditional rendering

--- REACT/react-app3/src/App.jsx
+++ REACT/react-app3/src/App.jsx
@@ -5,16 +5,21 @@ import './Stylesheet/index.scss'
 import Com1 from './Components/component1.jsx'
 import Comp2 from './Components/component2.jsx'
 function App() {
+   let [toShow, setToShow] = useState(false)
+   const handleClick = () => {
+    setToShow(!toShow)
+  }
   return <>
     <div>
       <div>
            <h3>this is app.jsx page content</h3>
       </div>
-      <Com1 />
-      <Comp2 />
-      <div className="btn btn-primary p-4">
+      {/* <Com1 />
+      <Comp2 /> */}
+      {toShow ? <Com1/> :<Comp2 />}
+      <div className="btn btn-primary p-4" onClick={handleClick}>
           Click Me
-        </div>
+      </div>
     </div>
   </>
 }
